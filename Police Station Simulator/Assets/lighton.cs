67bd772444c7bbe9dcb1169450cf7364fff3f8a6
FBarnes99 <ferranbarnes@gmail.com>
2019-11-03 01:05:28 +0100

    Light sistem update

---
+++ Police Station Simulator/Assets/lighton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lighton : MonoBehaviour
+{
+    LevelLoop level;
+   
+    
+    void Awake() {
+        level = GameObject.Find("Level").GetComponent<LevelLoop>();
+       // gameObject.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(level.day);
+        if (level.day) {
+            gameObject.GetComponent<Light>().enabled = false;
+            Debug.Log("DAY DEACTIVATE");
+        } else if (!level.day){
+            gameObject.GetComponent<Light>().enabled = true;
+            Debug.Log("Night ACTIVATE");
+        }
+    }
+}
--- Police Station Simulator/Assets/sun.cs
+++ Police Station Simulator/Assets/sun.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class sun : MonoBehaviour
 {
     LevelLoop level;
+    Light light;
+    Color day;
+    Color night;
     // Start is called before the first frame update
     void Awake()
     {
         level = GameObject.Find("Level").GetComponent<LevelLoop>();
+        light = GetComponent<Light>();
+
+        day.a = 255f/255f;
+        day.r = 255f/255f;
+        day.b = 196f/255f;
+        day.g = 239f/255f;
+
+        night.a = 255f/255f;
+        night.r = 165f/255f;
+        night.b = 255f/255f;
+        night.g = 246f/255f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.right, 4.5f * Time.deltaTime);
+        //transform.RotateAround(Vector3.zero, Vector3.right, 4.5f * Time.deltaTime);
+        if (light.intensity == 1F && level.day == false) {
+
+            light.intensity = 0.2F;
+            light.color = night;
+
+        } else if (light.intensity == 0.2F && level.day == true) {
+
+            light.intensity = 1F;
+            light.color = day;
+
+        }
+
     }
 }
