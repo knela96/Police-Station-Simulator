086c88c49d31870a9ae8392ffaee117341f9d43c
FBarnes99 <ferranbarnes@gmail.com>
2019-11-28 16:04:28 +0100

    More cells appears after 2 cycles

---
+++ Police Station Simulator/Assets/MapChanges.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapChanges : MonoBehaviour
+{
+    int countdown = 0;
+    LevelLoop level;
+    GameObject Levelaugm;
+    GameObject Wall1;
+    GameObject Wall2;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        level = GameObject.Find("Level").GetComponent<LevelLoop>();
+        Levelaugm = GameObject.Find("Phase2");
+        Wall1 = GameObject.Find("StWall");
+        Wall2 = GameObject.Find(" Phase2 Wall");
+       
+        Levelaugm.SetActive(false);
+        Wall1.SetActive(true);
+        Wall2.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (level.getCycle() >= 119.9f) { 
+            countdown++;
+        }
+
+        if (countdown == 4) {
+            Levelaugm.SetActive(true);
+            Wall1.SetActive(false);
+        }
+
+    }
+}
