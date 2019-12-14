d73dc5229974f3259f748a55bb1ef96af01ac322
knela96 <knela96@gmail.com>
2019-12-14 18:55:31 +0100

    added new Scene MainMenu

---
+++ Police Station Simulator/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public void START()
+    {
+        SceneManager.LoadScene("PoliceScene");
+    }
+
+    public void Credits()
+    {
+        Application.OpenURL("http://unity3d.com/");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
